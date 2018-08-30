3e1f2f1f23f47a204018947ed85bea9088d6a6b2
Kyle Lewis <kyle@kplewis.com>
2018-08-30 06:59:46 -0500

    Updated 1.10

--- 1.10.scm
+++ 1.10.scm
@@ -32,6 +32,14 @@ What are the values of the following expressions?
 ; (A 2 4)
 ; 2^16
 
+(define (f n) (A 0 n))
+;(f n) = 2n
+(define (g n) (A 1 n))
+;(g n) = 2^n
+(define (h n) (A 2 n))
+;(h n) = 2^(h (- n 1))
+(define (k n) (* 5 n n))
+
 
 
 
